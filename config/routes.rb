21480c046ac2f2aa66dccd8ff6ee8ee7fc353bea
qwertywla <qwerty.wla@gmail.com>
2019-05-18 02:10:31 -0700

    4.1 Scaffolding & Updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :products
   get 'simple_pages/bio'
   get 'simple_pages/contact'
   get 'simple_pages/portfolio'
+  get 'products/index'
   root 'simple_pages#index'
   get 'simple_pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
