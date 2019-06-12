8daf429bc223cabd23445f717a0ae43819fbcbc2
qwertywla <qwerty.wla@gmail.com>
2019-06-11 20:21:05 -0700

    Update 4.7

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,7 +30,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to '/simple_pages/landing_page' }
+        format.html { redirect_to '/simple_pages/index' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,11 +1,9 @@
 class SimplePagesController < ApplicationController
   def index
-    @feautured_product = Product.first
     @products = Product.limit(3)
   end
 
   def landing_page
-    @feautured_product = Product.first
     @products = Product.limit(3)
   end
 end
