f62676c7b09841485d70107c36c8401bf9dc008c
qwertywla <qwerty.wla@gmail.com>
2019-06-11 21:32:01 -0700

    Update controller

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,9 +1,11 @@
 class SimplePagesController < ApplicationController
   def index
     @products = Product.limit(3)
+    @featured_product = Product.first
   end
 
   def landing_page
     @products = Product.limit(3)
+    @featured_product = Product.first
   end
 end
