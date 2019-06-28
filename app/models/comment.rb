cb126d4a1a269d9685efa5909967cd7f2ffdb184
qwertywla <qwerty.wla@gmail.com>
2019-06-28 03:18:25 -0700

    5.9 Validation & Pagination

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,28 @@ class Comment < ApplicationRecord
   has_many :comments
     scope :rating_desc, -> { order(rating: :desc) }
     scope :rating_asc, -> { order(rating: :asc) }
+
+  validates :body, presence: true
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, numericality: { only_integer: true }
+
+  scope :rating_desc, -> { order(rating: :desc) }
+  scope :rating_asc, -> { order(rating: :asc) }
+end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @comment = @product.comments.new(comment_params)
+    @comment.user = current_user
+
+  respond_to do |format|
+    if @comment.save
+      format.html { redirect_to @product, notice: 'Review was created successfully.' }
+      format.json { render :show, status: :created, location: @product }
+    else
+      format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+      format.json { render json: @comment.errors, status: :unprocessable_entity }
+    end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
+
   has_many :orders
   has_many :comments
+
+  validates :name, presence: true
 
   def self.search(search_term)
     if Rails.env.production?
