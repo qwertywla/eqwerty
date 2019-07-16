7232028a002f2c791217d1771cea3b69c3e621a3
qwertywla <qwerty.wla@gmail.com>
2019-07-16 00:30:08 -0700

    6.11 Caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,13 +94,3 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://eqwerty.herokuapp.com' }
 end
-
-config.cache_store = :dalli_store,
-    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
-    {:username => ENV["MEMCACHIER_USERNAME"],
-     :password => ENV["MEMCACHIER_PASSWORD"],
-     :failover => true,
-     :socket_timeout => 1.5,
-     :socket_failure_delay => 0.2,
-     :down_retry_delay => 60
-    }
